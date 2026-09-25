00b8b2aea658456abfc8b5279e9b07850d28427f
bellamiecreative <bellamiecreative@gmail.com>
2026-09-25 08:58:50 +0300

    Update MainActivity.java

--- app/src/main/java/com/aibell/app/MainActivity.java
+++ app/src/main/java/com/aibell/app/MainActivity.java
@@ -2,38 +2,19 @@ package com.aibell.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
-        setContentView(webView);
+        TextView text = new TextView(this);
+        text.setText("AI Bell works!");
+        text.setTextSize(24);
+        text.setPadding(40, 40, 40, 40);
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        webView.loadUrl("file:///android_asset/index.html");
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        setContentView(text);
     }
 }
